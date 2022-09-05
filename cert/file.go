e9437fc271fdd93d1c01a1fb5ae3a4906919abfc
Lauris BH <lauris@nix.lv>
2022-09-05 19:15:52 +0300

    Add certificate helpers

---
+++ cert/file.go
@@ -0,0 +1,43 @@
+// Copyright 2022 Azugo. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package cert
+
+import (
+	"bytes"
+	"encoding/pem"
+	"os"
+)
+
+// LoadTLSCertificate loads a PEM-encoded certificate and private key from
+// the specified file.
+func LoadTLSCertificate(path string) ([]byte, []byte, error) {
+	raw, err := os.ReadFile(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	var cert, key []byte
+	for {
+		block, rest := pem.Decode(raw)
+		if block == nil {
+			break
+		}
+		if block.Type == PEMBlockCertificate {
+			out := &bytes.Buffer{}
+			if err = pem.Encode(out, block); err != nil {
+				return nil, nil, err
+			}
+			cert = out.Bytes()
+		} else if block.Type == PEMBlockRSAPrivateKey || block.Type == PEMBlockECPrivateKey {
+			out := &bytes.Buffer{}
+			if err = pem.Encode(out, block); err != nil {
+				return nil, nil, err
+			}
+			key = out.Bytes()
+		}
+		raw = rest
+	}
+
+	return cert, key, nil
+}
