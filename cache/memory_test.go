ec2c487ec5cf131680563cc592f865450aa1439f
Gatis Rīders <gatis.riders@gmail.com>
2023-02-18 11:27:56 +0200

    undo

--- cache/memory_test.go
+++ cache/memory_test.go
@@ -2,9 +2,6 @@ package cache
 
 import (
 	"context"
-	"crypto/rand"
-	"fmt"
-	"sync"
 	"testing"
 	"time"
 
@@ -12,98 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-
-func loadCacheValueById(ctx context.Context, key string) (any, error) {
-	// wait for 100ms to 700ms randomly to simulate slow loading
-	b := make([]byte, 1)
-	_, err := rand.Read(b)
-	if err != nil {
-		return nil, err
-	}
-	time.Sleep(time.Duration(100+int(b[0])%600) * time.Millisecond)
-	return fmt.Sprintf("value-%s", key), nil
-}
-
-var testKeys = []string {
-	"key1",
-	"key2",
-	"key3",
-	"key4",
-	"key5",
-	"key6",
-	"key7",
-}
-
-func TestMemoryCacheGetSetLoader(t *testing.T) {
-	cs := getRedisConnStr()
-	if cs == "" {
-		t.Skipped()
-		return
-	}
-	c := New(CacheType(RedisCache), ConnectionString(cs))
-	err := c.Start(context.TODO())
-	require.NoError(t, err)
-	defer c.Close()
-
-	i, err := Create[string](c, "test",
-		DefaultTTL(5*time.Minute),
-		Loader(loadCacheValueById),
-		MemoryCache,
-	)
-	i3, err := Create[string](c, "test23",
-		DefaultTTL(5*time.Minute),
-		Loader(loadCacheValueById),
-		MemoryCache,
-	)
-	i2, err := Create[string](c, "testRedis",
-		DefaultTTL(5*time.Minute),
-		Loader(loadCacheValueById),
-		RedisCache,
-	)
-	require.NoError(t, err)
-	var wg sync.WaitGroup
-
-	for _, key := range testKeys {
-		wg.Add(1)
-		go func(key string) {
-			val, err := i.Get(context.TODO(), key)
-			assert.NoError(t, err)
-			assert.Equal(t, fmt.Sprintf("value-%s", key), val)
-			firstVal, err := i.Get(context.TODO(), testKeys[0])
-			i.Set(context.TODO(), testKeys[0], fmt.Sprintf("value-%s", testKeys[0]))
-			assert.NoError(t, err)
-			assert.Equal(t, fmt.Sprintf("value-%s", testKeys[0]), firstVal)
-			i.Delete(context.TODO(), testKeys[0])
-			val, err = i2.Get(context.TODO(), key)
-			i.Set(context.TODO(), testKeys[0], fmt.Sprintf("value-%s", testKeys[0]))
-			assert.NoError(t, err)
-			assert.Equal(t, fmt.Sprintf("value-%s", key), val)
-			val, err = i3.Get(context.TODO(), key)
-			i3.Set(context.TODO(), testKeys[0], fmt.Sprintf("value-%s", testKeys[0]))
-			assert.NoError(t, err)
-			assert.Equal(t, fmt.Sprintf("value-%s", key), val)
-			wg.Done()
-		}(key)
-		wg.Add(1)
-		go func(key string) {
-			i.Set(context.TODO(), testKeys[0], fmt.Sprintf("value-%s", testKeys[0]))
-			assert.NoError(t, err)
-			val, err := i2.Get(context.TODO(), testKeys[0])
-			assert.Equal(t, fmt.Sprintf("value-%s", testKeys[0]), val)
-			i.Set(context.TODO(), testKeys[0], fmt.Sprintf("value-%s", testKeys[0]))
-			assert.NoError(t, err)
-			i3.Set(context.TODO(), testKeys[0], fmt.Sprintf("value-%s", testKeys[0]))
-			assert.NoError(t, err)
-			val, err = i3.Get(context.TODO(), testKeys[0])
-			assert.Equal(t, fmt.Sprintf("value-%s", testKeys[0]), val)
-			i3.Set(context.TODO(), testKeys[0], fmt.Sprintf("value-%s", testKeys[0]))
-			assert.NoError(t, err)
-			wg.Done()
-		}(key)
-	}
-	wg.Wait()
-
-}
 
 func TestMemoryCacheGetSet(t *testing.T) {
 	c := New(CacheType(MemoryCache))
