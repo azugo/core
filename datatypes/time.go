aacb2c4a2fa6dab97152260fe91d257f84671db9
Andrejs Prucevs <andrejs.prucevs@zzdats.lv>
2024-02-27 13:13:52 +0200

    Commonly used date data type

---
+++ datatypes/time.go
@@ -0,0 +1,63 @@
+package datatypes
+
+import (
+	"fmt"
+	"time"
+)
+
+// Time is a wrapper around time.Time that supports JSON marshaling.
+type Time time.Time
+
+func (t Time) String() string {
+	if time.Time(t).IsZero() {
+		return ""
+	}
+
+	return time.Time(t).Format(time.RFC3339Nano)
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + time.Time(t).Format(time.RFC3339Nano) + `"`), nil
+}
+
+func (t *Time) UnmarshalJSON(data []byte) error {
+	tt, err := time.Parse(`"`+time.RFC3339Nano+`"`, string(data))
+	if err != nil {
+		return err
+	}
+
+	*t = Time(tt)
+
+	return nil
+}
+
+// Date is a wrapper around time.Time that supports JSON marshaling to date.
+type Date time.Time
+
+func (t Date) String() string {
+	y, m, d := time.Time(t).Date()
+	return fmt.Sprintf("%04d-%02d-%02d", y, int(m), d)
+}
+
+func (t Date) MarshalJSON() ([]byte, error) {
+	return []byte(time.Time(t).Format(`"2006-01-02"`)), nil
+}
+
+func (t *Date) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+
+	tt, err := time.Parse(`"2006-01-02"`, string(data))
+	if err != nil {
+		// If parsing as pure date fails, try parsing as timestamp
+		tt, err = time.Parse(fmt.Sprintf(`"%s"`, time.RFC3339Nano), string(data))
+		if err != nil {
+			return err
+		}
+	}
+
+	*t = Date(tt)
+
+	return nil
+}
