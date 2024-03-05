bd11edb5ebcc3c9cf9ce21428b1a6d2aeb87197e
Andrejs Prucevs <andrejs.prucevs@zzdats.lv>
2024-03-05 12:10:06 +0200

    Handle nulls for date and time

--- datatypes/time.go
+++ datatypes/time.go
@@ -8,16 +8,20 @@ import (
 // Time is a wrapper around time.Time that supports JSON marshaling.
 type Time time.Time
 
-func (t Time) String() string {
-	if time.Time(t).IsZero() {
+func (t *Time) String() string {
+	if t == nil || time.Time(*t).IsZero() {
 		return ""
 	}
 
-	return time.Time(t).Format(time.RFC3339Nano)
+	return time.Time(*t).Format(time.RFC3339Nano)
 }
 
-func (t Time) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + time.Time(t).Format(time.RFC3339Nano) + `"`), nil
+func (t *Time) MarshalJSON() ([]byte, error) {
+	if t == nil {
+		return []byte(""), nil
+	}
+
+	return []byte(`"` + time.Time(*t).Format(time.RFC3339Nano) + `"`), nil
 }
 
 func (t *Time) UnmarshalJSON(data []byte) error {
@@ -34,13 +38,21 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 // Date is a wrapper around time.Time that supports JSON marshaling to date.
 type Date time.Time
 
-func (t Date) String() string {
-	y, m, d := time.Time(t).Date()
+func (t *Date) String() string {
+	if t == nil || time.Time(*t).IsZero() {
+		return ""
+	}
+
+	y, m, d := time.Time(*t).Date()
 	return fmt.Sprintf("%04d-%02d-%02d", y, int(m), d)
 }
 
-func (t Date) MarshalJSON() ([]byte, error) {
-	return []byte(time.Time(t).Format(`"2006-01-02"`)), nil
+func (t *Date) MarshalJSON() ([]byte, error) {
+	if t == nil {
+		return []byte(""), nil
+	}
+
+	return []byte(time.Time(*t).Format(`"2006-01-02"`)), nil
 }
 
 func (t *Date) UnmarshalJSON(data []byte) error {
