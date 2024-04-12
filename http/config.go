19da2b91e5e664a94b9c84b17677fdd3de0e1dd9
Lauris BH <lauris@nix.lv>
2024-04-12 16:15:33 +0300

    Add named HTTP client configuration validation

--- http/config.go
+++ http/config.go
@@ -1,8 +1,12 @@
 package http
 
+import (
+	"azugo.io/core/validation"
+)
+
 // NamedClient represents the configuration for the named client instance.
 type NamedClient struct {
-	BaseURI string `mapstructure:"base_uri" validate:"required"`
+	BaseURI string `mapstructure:"base_uri" validate:"required http_url"`
 }
 
 // Configuration represents the configuration for the HTTP client.
@@ -13,4 +17,15 @@ type Configuration struct {
 // apply the configuration to the options.
 func (c *Configuration) apply(o *options) {
 	o.Configuration = c
+}
+
+// Validate Metrics configuration section.
+func (c *Configuration) Validate(valid *validation.Validate) error {
+	for _, client := range c.Clients {
+		if err := valid.Struct(client); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
